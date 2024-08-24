4d2c42d0b90fc071ab9bab39e4a815abaf325a3a
Noble <nobleeugene2005@gmail.com>
2024-08-24 07:38:59 +0100

    added tests

--- swengine.tests/Scrapers/MoewallsTest.cs
+++ swengine.tests/Scrapers/MoewallsTest.cs
@@ -2,18 +2,15 @@ using Newtonsoft.Json;
 using swengine.desktop.Models;
 using swengine.desktop.Scrapers;
 using JsonSerializer = System.Text.Json.JsonSerializer;
-
-namespace swengine.tests.Scrapers;
-
 [TestClass]
-public class MotionBgsTests
+public class MoewallsTest
 {
     [TestMethod]
     public async Task LatestTest()
     {
         for (int i = 0; i <= 10; i++)
         {
-            var latest = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MotionBgsScraper.LatestAsync(i));
+            var latest = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MoewallsScraper.LatestOrSearchAsync(i,"latest"));
             
             Assert.IsNotNull(latest);
             Assert.IsInstanceOfType<List<WallpaperResponse>>(latest);
@@ -24,13 +21,13 @@ public class MotionBgsTests
     public async Task InfoTest()
     {
       
-            var latest = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MotionBgsScraper.LatestAsync(1));
-            for (int j = 0; j < latest.Count; j++)
-            {
-                var info = MotionBgsScraper.InfoAsync(latest[j].Src, latest[j].Title);
-                var info_object = JsonSerializer.Deserialize<Wallpaper>(await info);
-                Assert.IsNotNull(info_object);
-                Assert.IsInstanceOfType<Wallpaper>(info_object);
+        var latest = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MoewallsScraper.LatestOrSearchAsync(1,Function:"latest"));
+        for (int j = 0; j < latest.Count; j++)
+        {
+            var info = MoewallsScraper.InfoAsync(latest[j].Src, latest[j].Title);
+            var info_object = JsonSerializer.Deserialize<Wallpaper>(await info);
+            Assert.IsNotNull(info_object);
+            Assert.IsInstanceOfType<Wallpaper>(info_object);
         }
     }
 
@@ -40,7 +37,7 @@ public class MotionBgsTests
         string[] search_terms = new[] { "nature", "ocean", "space", "city" };
         foreach (var searchTerm in search_terms)
         {
-            var search = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MotionBgsScraper.SearchAsync(searchTerm,1));
+            var search = JsonSerializer.Deserialize<List<WallpaperResponse>>(await MoewallsScraper.LatestOrSearchAsync(1,"search",searchTerm));
             Assert.IsNotNull(search);
             Assert.IsInstanceOfType<List<WallpaperResponse>>(search);
         }
