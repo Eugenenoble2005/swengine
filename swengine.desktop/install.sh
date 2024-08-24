8c256d1fa871154d8f73c7e0914b32abe324e501
Noble <nobleeugene2005@gmail.com>
2024-08-24 09:38:03 +0100

    added shell installer

---
+++ swengine.desktop/install.sh
@@ -0,0 +1,7 @@
+dotnet publish --self-contained=true  --output dist
+chmod +x ./dist/swengine.desktop
+chmod +x swengine
+sudo cp  swengine /usr/bin
+sudo mkdir -p /usr/lib/swengine && sudo cp -r dist/* /usr/lib/swengine
+sudo cp swengine.desktop /usr/share/applications
+
