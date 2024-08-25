3691391a4c8635cc6e6eee1796c65183159aa71e
Noble <nobleeugene2005@gmail.com>
2024-08-25 03:06:25 +0100

    updated install script

--- swengine.desktop/install.sh
+++ swengine.desktop/install.sh
@@ -4,4 +4,4 @@ chmod +x swengine
 sudo cp  swengine /usr/bin
 sudo mkdir -p /usr/lib/swengine && sudo cp -r dist/* /usr/lib/swengine
 sudo cp swengine.desktop /usr/share/applications
-
+echo "Installation finished"
